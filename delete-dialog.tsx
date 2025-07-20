b518a5d0ecf9d7370d4f5f98a858db2211d8a11d
IQBAL FARHAN SYUHADA <53584619+iqbalfarhan@users.noreply.github.com>
2025-07-20 15:20:05 +0800

    Create delete-dialog.tsx

---
+++ delete-dialog.tsx
@@ -0,0 +1,59 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { em } from '@/lib/utils';
+import { Snippet } from '@/types';
+import { router } from '@inertiajs/react';
+import { Trash2 } from 'lucide-react';
+import { FC, PropsWithChildren, useState } from 'react';
+import { toast } from 'sonner';
+
+type Props = PropsWithChildren & {
+  snippet: Snippet;
+};
+
+const SnippetDeleteDialog: FC<Props> = ({ children, snippet }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleDelete = () => {
+    router.delete(route('snippet.destroy', snippet.id), {
+      preserveScroll: true,
+      onSuccess: () => {
+        toast.success('Snippet deleted successfully');
+        setOpen(false);
+      },
+      onError: (e) => toast.error(em(e)),
+    });
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete snippet and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDelete}>
+            <Trash2 />
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default SnippetDeleteDialog;
